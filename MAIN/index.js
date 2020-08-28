349ede7e02621d8b27066ad94a4eff68babdffb6
Dylan Gough <dylangough35@gmail.com>
2020-08-27 20:33:00 -0400

    index.js changes

--- MAIN/index.js
+++ MAIN/index.js
@@ -2,6 +2,7 @@ var inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const connection = require("../MAIN/connection.js");
 var mysql = require("mysql");
+const { exit } = require("process");
 
 function runSearch() {
     inquirer.prompt({
@@ -29,6 +30,57 @@ function runSearch() {
             case "Add Department":
                 addDepartment();
                 break;
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+            case "View All Of The Departments":
+                viewDepartments();
+                break;
+            case "View All Of The Roles":
+                viewRoles();
+                break;
+            case "View All Of Our Employees":
+                viewEmployees();
+                break;
+            case "Quit The Application":
+                exit();
+                break;
         }
+    });
+}
+function addEmployee() {
+    inquirer.prompt([
+        {
+        name: "firstName",
+        type: "input",
+        message: "What is the Employee's first name?",
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "What is the Employee's last name?",
+        },
+        {
+            name: "roleID",
+            type: "input",
+            message: "What is your Employee's Role ID?",
+        },
+        {
+            name: "managerID",
+            type: "input",
+            message: "What is their manager ID?",
+        },
+    ])
+    .then(function (answer) {
+        var query = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+        connection.query(
+            query,
+            [answer.firstName, answer.lastName, answer.roleID, answer.managerID],
+            function (err, res) {
+                if (err) throw err;
+                console.log('Successfully Added Employee to System: ${answer.firstName} ${answer.lastName}');
+                runSearch();
+            }
+        )
     })
 }
