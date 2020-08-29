4697933ac34358fe3264f2bb7c3a88d41d6bc13d
Dylan Gough <dylangough35@gmail.com>
2020-08-29 15:46:03 -0400

    init

--- MAIN/seed.sql
+++ MAIN/seed.sql
@@ -9,4 +9,9 @@ VALUES
 INSERT INTO role
 (title, salary, department_id)
 VALUES
-('Accountant')
+("Accountant", 80000, 2),
+("Eningeer", 95000, 1),
+("Lawyer", 120000, 3),
+("Sales Representitive", 60000, 4);
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id)
